df3f87d4e3742aada9c390f2c3f2517067e9f6c5
Jaime <jimmycc80@hotmail.com>
2014-11-15 23:19:46 -0500

    Created README.md file

--- src/com/appspot/senseampapp/senseamp/WriteByFriendActivity.java
+++ src/com/appspot/senseampapp/senseamp/WriteByFriendActivity.java
@@ -1,63 +1,64 @@
 package com.appspot.senseampapp.senseamp;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.content.Context;
 import android.os.Vibrator;
 import android.content.Intent;
+import android.widget.EditText;
 
-public class HomeActivity extends Activity {
+public class WriteByFriendActivity extends Activity {
 	private String2Vibrations str2vib = new String2Vibrations();
-
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_home);
+		setContentView(R.layout.activity_write_by_friend);
 	}
 
-	public void onClickWriteByMe(View v) {
-		// Call vibration pattern
-		long[] pattern = {0, 50, 150, 50, 150, 50, 150, 50};
-		Vibrator vib = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
-		vib.vibrate(pattern, -1);
-	}	
-	
-	public void onClickWriteByFriend(View v) {
-		// Call vibration pattern
-		long[] pattern = {0, 50, 150, 50, 150, 50};
-		Vibrator vib = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
-		vib.vibrate(pattern, -1);
-		
-		// Start write by me activity
-        Intent i = new Intent(HomeActivity.this, WriteByFriendActivity.class);
-        startActivity(i);
-
-        // close this activity
-        finish();
-	}	
-	
-	public void onClickPersonalize(View v) {
+	public void onClickHome(View v) {
 		// Call vibration pattern
 		long[] pattern = {0, 50};
 		Vibrator vib = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
 		vib.vibrate(pattern, -1);
-	}	
+		
+		// Start write by me activity
+        Intent i = new Intent(WriteByFriendActivity.this, HomeActivity.class);
+        startActivity(i);
+
+        // close this activity
+        finish();
+	}
 	
-	public void onClickBooks(View v) {
-		// Call vibration pattern
+	public void onClickPlay(View v) {
 		long[] pattern = {0, 50, 150, 50};
-		Vibrator vib = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
-		vib.vibrate(pattern, -1);
-	}	
-	
+		String stringToPlay;
+		EditText inputText;
+		Vibrator vib;
+		
+		// Call vibration pattern (conflicts with string to play)
+		//vib = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+		//vib.vibrate(pattern, -1);
+		
+		// Get pattern from edit text
+		inputText = (EditText) findViewById(R.id.textToPlay);
+		stringToPlay = inputText.getText().toString();
+		
+		// Get instance of Vibrator from current Context
+		vib = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+		pattern = str2vib.getVibrations(stringToPlay);
+		vib.vibrate(pattern, -1);		
+	}
 	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.home, menu);
+		getMenuInflater().inflate(R.menu.write_by_friend, menu);
 		return true;
 	}
 
